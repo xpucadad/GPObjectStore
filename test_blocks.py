37a36720438f82515669c3704c960f909c8ca26b
xpucadad <github@kmreed.net>
2016-03-25 16:03:12 -0700

    In the test_block.py module, change the pbh
    Changed the previous block hash in the 1st block back to bytes(32)
    instead of sha256x2(bytes(32)). This matches what I did with the newer
    tests and allows easy checking for the end when walking the block chain
    from the latest block to the 1st block.

--- test_blocks.py
+++ test_blocks.py
@@ -6,8 +6,8 @@ from blocks import Block, BlockFactory
 
 class BlocksTestCase(unittest.TestCase):
     original_content = 'The quick brown fox jumps over the lazy dog.'
-    expected_header_digest = '034670e383dbcd651eefc941685dfd3001abe2eed9ef675876c529eaf784f91d'
-    expected_full_block = '800000000100000067050eeb5f95abf57449d92629dcf69f80c26247e207ad006a862d1e4e6498ffa51a910ecba8a599555b32133bf1829455d55fe576677b49cb561d874077385c25e9ed562003a30c6f0000002c00000054686520717569636b2062726f776e20666f78206a756d7073206f76657220746865206c617a7920646f672e'
+    expected_header_digest = '004a6f56702e01bbd97f860de1d39141219e3ef7fdfb883559ba597748cdaf10'
+    expected_full_block = '80000000010000000000000000000000000000000000000000000000000000000000000000000000a51a910ecba8a599555b32133bf1829455d55fe576677b49cb561d874077385c25e9ed562003a30c790000002c00000054686520717569636b2062726f776e20666f78206a756d7073206f76657220746865206c617a7920646f672e'
 
     def setUp(self):
         self.byte_content = self.original_content.encode()
@@ -17,7 +17,8 @@ class BlocksTestCase(unittest.TestCase):
     def test_newBlock(self, mock_object):
         # Create a new object and farm it
         test_block = self.factory.createNew(self.byte_content)
-        pbh = test_block.sha256x2(b'0')
+#        pbh = test_block.sha256x2(b'0')
+        pbh = bytes(32)
         header_digest = test_block.farm(pbh)
 
         # Verify the header digest from farming to
