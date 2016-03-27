d0b9f190cbfa6aab2731ef5dd43478a971c8cc20
xpucadad <github@kmreed.net>
2016-03-27 16:00:16 -0700

    Finished the test for converting content to new blocks.

--- contentprocessing.py
+++ contentprocessing.py
@@ -2,6 +2,9 @@ import logging
 import queue
 import threading
 
+from blocks import Block, BlockFactory
+from blockchain import BlockChain
+
 class ProcessContent(threading.Thread):
 
     def __init__(self, group=None, target=None, name=None, args=(), kwargs={}):
@@ -11,14 +14,28 @@ class ProcessContent(threading.Thread):
         # Save the thread name and keyword arguments
         self.name = name
         self.content_queue = kwargs['content_queue']
+        self.block_queue = kwargs['block_queue']
+        self.block_chain = kwargs['blockchain']
+        self.factory = BlockFactory()
 
     def run(self):
         logging.debug('ProcessContent thread starting')
 
         while True:
             msg = self.content_queue.get()
-            if msg is None: break
+            if msg is None:
+                self.block_queue.put(None)
+                break
             logging.debug(msg)
+
+            # Generate and farm a block from the content
+            block = self.factory.createNew(msg)
+            pbhd = self.block_chain.getLastBlockDigest()
+            hd = block.farm(pbhd)
+
+            # Return the the block as raw bytes
+            self.block_queue.put(block.toBytes())
+
             self.content_queue.task_done()
 
         logging.debug('ProcessContent thread ending')
--- test_contentprocessing.py
+++ test_contentprocessing.py
@@ -1,35 +1,71 @@
 import logging
 import logging.config
 import queue
+import struct
 import test_utils
 import threading
 import time
 import unittest
 
+from blocks import Block
+from blockchain import BlockChain
 from contentprocessing import ProcessContent
 
 class ProcessContentTestCase(unittest.TestCase):
+    filename = 'test_contentprocessing.dat'
+
     def setUp(self):
+        self.blockchain = BlockChain()
+        self.content_queue = queue.Queue()
+        self.block_queue = queue.Queue()
         logging.debug('setUp')
 
     def tearDown(self):
+        del self.block_queue
+        del self.content_queue
+        del self.blockchain
         logging.debug('tearDown')
 
     def test_01(self):
         logging.debug('Start test_01')
-        content_queue = queue.Queue()
         kwargs = {}
-        kwargs['content_queue'] = content_queue
+        kwargs['content_queue'] = self.content_queue
+        kwargs['block_queue'] = self.block_queue
+        kwargs['blockchain' ] = self.blockchain
 
         pct = ProcessContent(name='ProcessContentThread', kwargs=kwargs)
 
         pct.start()
-        content = (b'abc', b'def', b'Love is in the air!')
-        for i in content:
-            content_queue.put(i)
+        # For checking purposes, each content should be unique
+        input_content = (b'abc', b'def', b'Love is in the air!')
+        for i in input_content:
+            self.content_queue.put(i)
 
-        content_queue.put(None)
+        self.content_queue.put(None)
         pct.join()
+
+        # Now get all the generated blocks
+        received_content = {}
+        while True:
+            raw_block = self.block_queue.get()
+            if (raw_block is None):
+                break
+            # The block was queued as raw bytes; reconstruct it
+            block = Block()
+            block.parseFromBytes(raw_block)
+            self.assertTrue(block.validateHeaderDigest(), 'Bad header digest')
+            logging.debug(block.toBytes().hex())
+            block_content = block.getContent()
+            received_content[block_content] = True
+            logging.info(block_content)
+            self.block_queue.task_done()
+
+        # Verify that there is one block with content for
+        # each content input.
+        for c in input_content:
+            print(c)
+            self.assertTrue(c in received_content, 'No block generated for ' + c.decode('utf-8'))
+
         logging.debug('End test_01')
 
 def suite():
