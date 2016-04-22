181410d07a1c9dc437af9f86b565b27df5980f9f
xpucadad <github@kmreed.net>
2016-04-22 10:58:04 -0700

    Checkpoint

--- accounts.py
+++ accounts.py
@@ -5,16 +5,20 @@ import time
 
 class Account():
     version = struct.pack('I', 1)
+    public_key_size = 32    # 256 bits
+    private_key_size = 32   # 256 bits
 
     def __init__(self):
         self.private_key = bytes(32)
         self.public_key = self._generatePublicKey()
+        self.address = self._generateAddress()
 
     def generateKeys(self):
         seed = struct.pack('I', int(time.time()))
         self.private_key = hashes.sha256x2(seed)
         logging.debug('generateKeys: private key: %s', self.private_key.hex())
         self._generatePublicKey()
+        self._generateAddress()
 
     def loadFromPublicKey(self, bytestream):
         pass
@@ -23,6 +27,11 @@ class Account():
         self.public_key = self.private_key
         logging.debug('_generatePublicKey: public_key: %s', self.public_key.hex())
         return self.public_key
+
+    def _generateAddress(self):
+        self.address = hashes.ripemd160xsha256(self.public_key)
+        logging.debug('_generateAddress: address: %s',self.address.hex())
+        return self.address
 
     def getPublicKey(self):
         return self.public_key
--- hashes.py
+++ hashes.py
@@ -1,8 +1,10 @@
 import hashlib
 
 def sha256x2(data):
-    hashobj = hashlib.sha256(data)
-    d1 = hashobj.digest()
-    hashobj = hashlib.sha256(d1)
-    digest = hashobj.digest()
+    digest = hashlib.sha256(hashlib.sha256(data).digest()).digest()
+    return digest
+
+def ripemd160xsha256(data):
+    h256_dig = hashlib.new('sha256', data).digest()
+    digest = hashlib.new('ripemd160', h256_dig).digest()
     return digest
